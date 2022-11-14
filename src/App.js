21c79113cf3153c8165acf12cf0aede7126e24a5
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-14 19:28:34 +0100

    Opravil som si chybu a prepísal meno componentu na <MyAcordion />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import MyAccordion from './Component/MyAccordion';
 import data from './data';
 
 function App() {
   return (
     <main >
-
+      <MyAccordion data={data} />
     </main>
   );
 }
