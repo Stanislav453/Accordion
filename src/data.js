bb05b888e71b1e9643dafd523eb08aa454366864
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-14 19:09:49 +0100

    Pridal som data do aplikacie

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = [{title:"Topiclounge",desc:"venenatis lacinia aenean sit amet justo morbi ut odio cras mi pede malesuada in imperdiet et commodo vulputate justo in blandit ultrices enim"},
+{title:"Yoveo",desc:"mattis pulvinar nulla pede ullamcorper augue a suscipit nulla elit ac nulla sed vel enim sit amet nunc viverra dapibus"},
+{title:"Aimbu",desc:"quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse"},
+{title:"Gabtype",desc:"egestas metus aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum"},
+{title:"Tagopia",desc:"integer a nibh in quis justo maecenas rhoncus aliquam lacus morbi quis tortor id nulla ultrices aliquet maecenas leo odio condimentum id luctus nec molestie sed justo pellentesque"},
+{title:"Yacero",desc:"massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante"},
+{title:"Rhynyx",desc:"in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer aliquet massa id lobortis convallis tortor risus dapibus augue vel accumsan tellus nisi"},
+{title:"Yambee",desc:"eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo"}]
+
+export default data 
