864e232c31d9c90d7449d475f306cdcf33a26a3c
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-15 22:32:03 +0100

    Doprogramoval som Header

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>My Accordion</h1>
+            <div className="underline"></div>
+        </header>
+    )
+}
+
+export default Header
